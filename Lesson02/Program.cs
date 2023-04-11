e7cba729a331bd0e92cb40528cc9eb6316e7a030
Chris <chriscrossje2001@gmail.com>
2023-04-11 12:33:30 +0200

    Exercise 1 & 2 of chapter 2

---
+++ Lesson02/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Lesson02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rect = new Rectangle(10.0, 20.0);
+            double area = rect.GetArea();
+            // {0} contains the value of area
+            Console.WriteLine("The area of the rectangle: {0}", area);
+        }
+    }
+
+    class Rectangle
+    {
+        private double length;
+        private double width;
+
+        public Rectangle(double l, double w)
+        {
+            this.length = l;
+            this.width = w;
+        }   
+
+        public double GetArea()
+        {
+            return length * width;
+        }
+    }
+}
+

4ff470b7537b5c2fed40a44ac817eca9055314bc
Chris <chriscrossje2001@gmail.com>
2023-04-15 00:55:30 +0200

    Added the exercises of chapter 2

---
+++ Events/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Lesson02 {
+    class Program {
+        static void Main(string[] args) {
+            Rectangle r = new Rectangle();
+            r.Changed += new EventHandler(r_Changed);
+            r.Length = 20;
+        }
+        static void r_Changed(object sender, EventArgs e) {
+            Rectangle r = (Rectangle)sender;
+            Console.WriteLine(
+                "Value Changed: Length = {0}",
+                r.Length);
+        }
+    }
+
+    class Rectangle {
+        public event EventHandler Changed;
+        private double length;
+        public double Length {
+            get {
+                return this.length;
+            }
+            set {
+                this.length = value;
+                Changed(this, EventArgs.Empty);
+            }
+        }
+    }
+}
+
